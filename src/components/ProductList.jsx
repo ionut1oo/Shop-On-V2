fc400c855a1cb8620a6130b99b1093c81787ba2c
ionut ciornei <Pantelimon194@gmail.com>
2024-01-03 20:02:35 +0200

    added alert messaje

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addToCart } from "../features/cartSlice";
+import { addToCart } from '../features/cartSlice';
 import Data from '../data/Data.json';
 import { Link } from 'react-router-dom';
-import "../styles/productList.css";
+import '../styles/productList.css';
 
 const ProductList = ({ searchTerm }) => {
     const dispatch = useDispatch();
-    
+    const [showAlert, setShowAlert] = useState(false);
+
     const handleAddToCart = (e, selectedItem) => {
         e.preventDefault();
         e.stopPropagation();
         const item = { ...selectedItem, quantity: 1 };
         dispatch(addToCart(item));
+
+        setShowAlert(true);
+
+        setTimeout(() => {
+            setShowAlert(false);
+        }, 10000);
     };
-    
+
     const maxLength = 55;
 
-    const filteredData = Data.filter(item =>
+    const filteredData = Data.filter((item) =>
         item.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     return (
         <div className="cards">
-            {filteredData.map(item => (
+            {filteredData.map((item) => (
                 <div key={item.id} className="card">
-                    <Link to={`/products/${item.id}`} key={item.id} className='card-link'>
+                    <Link to={`/products/${item.id}`} key={item.id} className="card-link">
                         <img src={item.imgUrl} alt="cardImage" className="card__image" />
                         <h4 className="card__name">
                             {item.name.substring(0, maxLength)}
-                            {item.name.length > maxLength ? "..." : ""}
+                            {item.name.length > maxLength ? '...' : ''}
                         </h4>
                     </Link>
                     <p className="card__price">{item.price}</p>
@@ -41,6 +48,11 @@ const ProductList = ({ searchTerm }) => {
                     </button>
                 </div>
             ))}
+            {showAlert && (
+                <div className="custom-alert">
+                    The product has been successfully added to the basket
+                </div>
+            )}
         </div>
     );
 };
